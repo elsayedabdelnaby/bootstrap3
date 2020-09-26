051dabfbf12fae769f2576732c5d609dfe1e6e86
elaraby <elsayed.fcis.1@gmail.com>
2020-09-26 22:21:24 +0200

    active update theme color using jquery

--- js/plugin.js
+++ js/plugin.js
@@ -6,4 +6,16 @@ $(document).ready(function(){
     $('.gear-check').click(function(){
         $('.color-option').fadeToggle();
     });
+
+    //Change Theme Color On Click
+    var colorLi = $('.color-option ul li');
+    colorLi
+        .eq(0).css("backgroundColor", "#e41b17").end()
+        .eq(1).css("backgroundColor", "#df10d4").end()
+        .eq(2).css("backgroundColor", "#1907b4").end()
+        .eq(3).css("backgroundColor", "#c7c408ce");
+    
+    colorLi.click(function(){
+        $("link[href*='theme']").attr("href", $(this).attr('data-value'));
+    })
 });
